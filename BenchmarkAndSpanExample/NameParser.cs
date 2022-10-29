8a099ef198081c0142cea113bf23a9d3b8f097de
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-10-29 21:36:52 +0200

    Initial commit

---
+++ BenchmarkAndSpanExample/NameParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace BenchmarkAndSpanExample
+{
+    public class NameParser
+    {
+        public string GetLastName(string fullName)
+        {
+            var names = fullName.Split(" ");
+
+            var lastName = names.LastOrDefault();
+
+            return lastName ?? string.Empty;
+        }
+    }
+}
---
+++ BenchmarkAndSpanExample/NameParserBenchmarks.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BenchmarkAndSpanExample
+{
+    [MemoryDiagnoser]
+    public class NameParserBenchmarks
+    {
+        private const string FullName = "Kristijan KPR Prematarov";
+        private static readonly NameParser Parser = new NameParser();
+
+        [Benchmark(Baseline = true)]
+        public void GetLastName()
+        {
+            Parser.GetLastName(FullName);
+        }
+    }
+}
